b9d28169b08af9705cff419749f1e89ebf094a63
yukiho11511 <ia15illust@gmail.com>
2023-06-29 12:06:34 +0900

    リファクタリング　ファイルを分割できるように修正

--- src/code.ts
+++ src/code.ts
@@ -23,15 +23,13 @@ function detachAllInstances(nodes: ReadonlyArray<SceneNode>) {
   for (let i = 0; i < nodes.length; i++) {
     const node: SceneNode = nodes[i];
     // 要素がインスタンスであれば、インスタンスを切り離す
-    if (node.type === "INSTANCE") {
+    if (node.type == "INSTANCE") {
       let frame: FrameNode = node.detachInstance();
       // インスタンスを切り離した後のフレーム内のインスタンスを探し、切り離す
       detachAllInstances(frame.children);
     }
     // 子要素を持つタイプであれば、子要素に対して処理を繰り返す
-    if (node.type === "BOOLEAN_OPERATION" || node.type === "COMPONENT"
-      || node.type === "COMPONENT_SET" || node.type === "FRAME"
-      || node.type === "GROUP" || node.type === "SECTION") {
+    if ("children" in node) {
       detachAllInstances(node.children);
     }
   }
